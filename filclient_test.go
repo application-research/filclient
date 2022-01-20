cf85224f37355e15d9c85608d7e7d4a6abbc6e1f
Elijah Seed-Arita <elijaharita@gmail.com>
2022-01-20 10:31:40 -1000

    prepare to add more tests

--- filclient_test.go
+++ filclient_test.go
@@ -28,7 +28,6 @@ import (
 	leveldb "github.com/ipfs/go-ds-leveldb"
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
 	chunk "github.com/ipfs/go-ipfs-chunker"
-	logging "github.com/ipfs/go-log/v2"
 	"github.com/ipfs/go-merkledag"
 	"github.com/ipfs/go-unixfs/importer"
 	"github.com/multiformats/go-multiaddr"
@@ -64,214 +63,178 @@ func (reader *DummyDataGen) Read(p []byte) (int, error) {
 
 	return i, nil
 }
-
-func TestMain(t *testing.T) {
-
-	logging.SetLogLevel("*", "error")
-
+func TestStorage(t *testing.T) {
 	app := cli.NewApp()
-
 	app.Action = func(cctx *cli.Context) error {
+		_, miner, _, fc, closer := initEnsemble(t, cctx)
+		defer closer()
 
 		ctx := cctx.Context
 
-		// Test network initialization
-
-		fmt.Printf("Initializing test network...\n")
-
-		kit.EnableLargeSectors(t)
-		kit.QuietMiningLogs()
-		client, miner, ensemble := kit.EnsembleMinimal(t,
-			kit.ThroughRPC(),        // so filclient can talk to it
-			kit.MockProofs(),        // we don't care about proper sealing/proofs
-			kit.SectorSize(512<<20), // 512MiB sectors
-		)
-		ensemble.InterconnectAll().BeginMining(50 * time.Millisecond)
-
-		// set the *optional* on-chain multiaddr
-		// the mind boggles: there is no API call for that - got to assemble your own msg
-		{
-			minfo, err := miner.FullNode.StateMinerInfo(ctx, miner.ActorAddr, lotustypes.EmptyTSK)
-			if err != nil {
-				return err
-			}
-
-			maddrNop2p, _ := multiaddr.SplitFunc(miner.ListenAddr, func(c multiaddr.Component) bool {
-				return c.Protocol().Code == multiaddr.P_P2P
-			})
-
-			params, aerr := lotusactors.SerializeParams(&filminer.ChangeMultiaddrsParams{NewMultiaddrs: [][]byte{maddrNop2p.Bytes()}})
-			if aerr != nil {
-				return aerr
-			}
-
-			_, err = miner.FullNode.MpoolPushMessage(ctx, &lotustypes.Message{
-				To:     miner.ActorAddr,
-				From:   minfo.Worker,
-				Value:  lotustypes.NewInt(0),
-				Method: filbuiltin.MethodsMiner.ChangeMultiaddrs,
-				Params: params,
-			}, nil)
-			if err != nil {
-				return err
-			}
-		}
-
-		fmt.Printf("Test client fullnode running on %s\n", client.ListenAddr)
-		os.Setenv("FULLNODE_API_INFO", client.ListenAddr.String())
-
-		client.WaitTillChain(ctx, kit.BlockMinedBy(miner.ActorAddr))
-
-		// FilClient initialization
-		fmt.Printf("Initializing filclient...\n")
-
-		// give filc the pre-funded wallet from the client
-		ki, err := client.WalletExport(ctx, client.DefaultKey.Address)
-		require.NoError(t, err)
-		lr, err := lotusrepo.NewMemory(nil).Lock(lotusrepo.Wallet)
-		require.NoError(t, err)
-		ks, err := lr.KeyStore()
-		require.NoError(t, err)
-		wallet, err := wallet.NewWallet(ks)
-		require.NoError(t, err)
-		_, err = wallet.WalletImport(ctx, ki)
-		require.NoError(t, err)
-
-		h, err := ensemble.Mocknet().GenPeer()
-		if err != nil {
-			t.Fatalf("Could not gen p2p peer: %v", err)
-		}
-		ensemble.Mocknet().LinkAll()
-		api, closer := initAPI(t, cctx)
-		bs := initBlockstore(t)
-		ds := initDatastore(t)
-		defer closer()
-		fc, err := NewClient(h, api, wallet, client.DefaultKey.Address, bs, ds, t.TempDir())
-		if err != nil {
-			t.Fatalf("Could not initialize FilClient: %v", err)
-		}
-
-		// Test data initialization
-		fmt.Printf("Initializing test data...\n")
-
-		bserv := blockservice.New(bs, nil)
+		bserv := blockservice.New(fc.blockstore, nil)
 		dserv := merkledag.NewDAGService(bserv)
 
-		const gib = 128 * 1024 * 1024
-		spl := chunk.DefaultSplitter(NewDummyDataGen(gib))
+		spl := chunk.DefaultSplitter(NewDummyDataGen(128 << 20))
 
 		obj, err := importer.BuildDagFromReader(dserv, spl)
 		if err != nil {
 			t.Fatalf("Could not build test data DAG: %v", err)
 		}
 
-		// Tests
-		fmt.Printf("Starting tests...\n")
+		addr, err := miner.ActorAddress(ctx)
+		fmt.Printf("Actor address: %s\n", addr)
+		require.NoError(t, err)
 
-		t.Run("storage", func(t *testing.T) {
-			addr, err := miner.ActorAddress(ctx)
-			fmt.Printf("Actor address: %s\n", addr)
-			if err != nil {
-				t.Fatalf("Could not get miner address: %s", err)
-			}
+		fmt.Printf("Testing storage deal for miner %s\n", addr)
 
-			fmt.Printf("Testing storage deal for miner %s\n", addr)
+		ask, err := fc.GetAsk(ctx, addr)
+		require.NoError(t, err)
 
-			ask, err := fc.GetAsk(ctx, addr)
-			if err != nil {
-				t.Fatalf("Failed to get ask from miner: %v", err)
-			}
+		_, err = fc.LockMarketFunds(
+			ctx,
+			lotustypes.FIL(lotustypes.NewInt(1000000000000000)), // FIXME - no idea what's reasonable
+		)
+		require.NoError(t, err)
 
-			_, err = fc.LockMarketFunds(
-				ctx,
-				lotustypes.FIL(lotustypes.NewInt(1000000000000000)), // FIXME - no idea what's reasonable
-			)
-			require.NoError(t, err)
+		proposal, err := fc.MakeDeal(ctx, addr, obj.Cid(), ask.Ask.Ask.Price, 0, 2880*365, false)
+		require.NoError(t, err)
 
-			proposal, err := fc.MakeDeal(ctx, addr, obj.Cid(), ask.Ask.Ask.Price, 0, 2880*365, false)
-			if err != nil {
-				t.Fatalf("Failed to make deal with miner: %v", err)
-			}
+		proposalRes, err := fc.SendProposal(ctx, proposal)
+		require.NoError(t, err)
 
-			proposalRes, err := fc.SendProposal(ctx, proposal)
-			if err != nil {
-				t.Fatalf("Sending proposal failed: %v", err)
-			}
+		switch proposalRes.Response.State {
+		case storagemarket.StorageDealWaitingForData, storagemarket.StorageDealProposalAccepted:
+		default:
+			t.Fatalf("Test deal was not accepted")
+		}
 
-			switch proposalRes.Response.State {
-			case storagemarket.StorageDealWaitingForData, storagemarket.StorageDealProposalAccepted:
-			default:
-				t.Fatalf("Test deal was not accepted")
-			}
+		var chanid *datatransfer.ChannelID
+		var chanidLk sync.Mutex
+		res := make(chan error, 1)
 
-			var chanid *datatransfer.ChannelID
-			var chanidLk sync.Mutex
-			res := make(chan error, 1)
-
-			finish := func(err error) {
-				select {
-				case res <- err:
-				default:
-				}
-			}
-
-			unsubscribe := fc.SubscribeToDataTransferEvents(func(event datatransfer.Event, state datatransfer.ChannelState) {
-				chanidLk.Lock()
-				chanidCopy := *chanid
-				chanidLk.Unlock()
-
-				// Skip messages not related to this channel
-				if state.ChannelID() != chanidCopy {
-					return
-				}
-
-				switch event.Code {
-				case datatransfer.CleanupComplete: // FIXME previously this was waiting for a code that would never come - not sure if CleanupComplete is right here....
-					finish(nil)
-				case datatransfer.Error:
-					finish(fmt.Errorf("data transfer failed"))
-				}
-			})
-			defer unsubscribe()
-
-			chanidLk.Lock()
-			chanid, err = fc.StartDataTransfer(ctx, miner.ActorAddr, proposalRes.Response.Proposal, obj.Cid())
-			chanidLk.Unlock()
-			if err != nil {
-				t.Fatalf("Failed to start data transfer")
-			}
-
+		finish := func(err error) {
 			select {
-			case err := <-res:
-				if err != nil {
-					t.Fatalf("Data transfer error: %v", err)
-				}
-			case <-cctx.Done():
+			case res <- err:
+			default:
+			}
+		}
+
+		unsubscribe := fc.SubscribeToDataTransferEvents(func(event datatransfer.Event, state datatransfer.ChannelState) {
+			chanidLk.Lock()
+			chanidCopy := *chanid
+			chanidLk.Unlock()
+
+			// Skip messages not related to this channel
+			if state.ChannelID() != chanidCopy {
+				return
 			}
 
-			if err := fc.dataTransfer.CloseDataTransferChannel(ctx, *chanid); err != nil {
-				t.Fatalf("Failed to close data transfer channel")
+			switch event.Code {
+			case datatransfer.CleanupComplete: // FIXME previously this was waiting for a code that would never come - not sure if CleanupComplete is right here....
+				finish(nil)
+			case datatransfer.Error:
+				finish(fmt.Errorf("data transfer failed"))
 			}
 		})
+		defer unsubscribe()
 
-		t.Run("retrieval", func(t *testing.T) {
-			// qres, err := fc.RetrievalQuery(ctx, miner.ActorAddr, obj.Cid())
-			// if err != nil {
-			// 	t.Fatalf("Retrieval query failed: %v", err)
-			// }
+		chanidLk.Lock()
+		chanid, err = fc.StartDataTransfer(ctx, miner.ActorAddr, proposalRes.Response.Proposal, obj.Cid())
+		chanidLk.Unlock()
+		if err != nil {
+			t.Fatalf("Failed to start data transfer")
+		}
 
-			// fmt.Printf("Retrieval query result: %#v", qres)
-		})
+		select {
+		case err := <-res:
+			if err != nil {
+				t.Fatalf("Data transfer error: %v", err)
+			}
+		case <-cctx.Done():
+		}
+
+		if err := fc.dataTransfer.CloseDataTransferChannel(ctx, *chanid); err != nil {
+			t.Fatalf("Failed to close data transfer channel")
+		}
 
 		return nil
-	}
-
-	if err := app.Run([]string{""}); err != nil {
-		t.Fatalf("App failed: %v", err)
 	}
 }
 
 // -- Setup functions
+
+// Create and set up an ensemble with linked filclient
+func initEnsemble(t *testing.T, cctx *cli.Context) (*kit.TestFullNode, *kit.TestMiner, *kit.Ensemble, *FilClient, func()) {
+
+	fmt.Printf("Initializing test network...\n")
+
+	kit.EnableLargeSectors(t)
+	kit.QuietMiningLogs()
+	client, miner, ensemble := kit.EnsembleMinimal(t,
+		kit.ThroughRPC(),        // so filclient can talk to it
+		kit.MockProofs(),        // we don't care about proper sealing/proofs
+		kit.SectorSize(512<<20), // 512MiB sectors
+	)
+	ensemble.InterconnectAll().BeginMining(50 * time.Millisecond)
+
+	// set the *optional* on-chain multiaddr
+	// the mind boggles: there is no API call for that - got to assemble your own msg
+	{
+		minfo, err := miner.FullNode.StateMinerInfo(cctx.Context, miner.ActorAddr, lotustypes.EmptyTSK)
+		require.NoError(t, err)
+
+		maddrNop2p, _ := multiaddr.SplitFunc(miner.ListenAddr, func(c multiaddr.Component) bool {
+			return c.Protocol().Code == multiaddr.P_P2P
+		})
+
+		params, aerr := lotusactors.SerializeParams(&filminer.ChangeMultiaddrsParams{NewMultiaddrs: [][]byte{maddrNop2p.Bytes()}})
+		require.NoError(t, aerr)
+
+		_, err = miner.FullNode.MpoolPushMessage(cctx.Context, &lotustypes.Message{
+			To:     miner.ActorAddr,
+			From:   minfo.Worker,
+			Value:  lotustypes.NewInt(0),
+			Method: filbuiltin.MethodsMiner.ChangeMultiaddrs,
+			Params: params,
+		}, nil)
+		require.NoError(t, err)
+	}
+
+	fmt.Printf("Test client fullnode running on %s\n", client.ListenAddr)
+	os.Setenv("FULLNODE_API_INFO", client.ListenAddr.String())
+
+	client.WaitTillChain(cctx.Context, kit.BlockMinedBy(miner.ActorAddr))
+
+	// FilClient initialization
+	fmt.Printf("Initializing filclient...\n")
+
+	// give filc the pre-funded wallet from the client
+	ki, err := client.WalletExport(cctx.Context, client.DefaultKey.Address)
+	require.NoError(t, err)
+	lr, err := lotusrepo.NewMemory(nil).Lock(lotusrepo.Wallet)
+	require.NoError(t, err)
+	ks, err := lr.KeyStore()
+	require.NoError(t, err)
+	wallet, err := wallet.NewWallet(ks)
+	require.NoError(t, err)
+	_, err = wallet.WalletImport(cctx.Context, ki)
+	require.NoError(t, err)
+
+	h, err := ensemble.Mocknet().GenPeer()
+	if err != nil {
+		t.Fatalf("Could not gen p2p peer: %v", err)
+	}
+	ensemble.Mocknet().LinkAll()
+	api, closer := initAPI(t, cctx)
+	bs := initBlockstore(t)
+	ds := initDatastore(t)
+	fc, err := NewClient(h, api, wallet, client.DefaultKey.Address, bs, ds, t.TempDir())
+	if err != nil {
+		t.Fatalf("Could not initialize FilClient: %v", err)
+	}
+
+	return client, miner, ensemble, fc, closer
+}
 
 func initAPI(t *testing.T, cctx *cli.Context) (api.Gateway, jsonrpc.ClientCloser) {
 	api, closer, err := lcli.GetGatewayAPI(cctx)
